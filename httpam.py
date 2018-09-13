3dc5f4e99dad9245b0efa45998711697643ebf9f
Richard Neumann <mail@richard-neumann.de>
2018-09-13 20:17:20 +0200

    init.

---
+++ httpam.py
@@ -0,0 +1,152 @@
+"""A web service authentication API using PAM."""
+
+from datetime import datetime
+from enum import Enum
+from functools import lru_cache, wraps
+from json import loads
+from pwd import getpwnam, struct_passwd
+from typing import NamedTuple
+from uuid import uuid4, UUID
+
+from pam import authenticate
+
+
+__all__ = ['SessionManager']
+
+
+CONFIG_FILE = '/etc/httpam.conf'
+DEFAULT_CONFIG = {
+    'allow_root': 'false',
+    'min_uid': str(1000),
+    'login_policy': 'override'
+}
+
+
+class InvalidUserNameOrPassword(Exception):
+    """Indicates an unsuccessful login attempt."""
+
+    pass
+
+
+class AlreadyLoggedIn(Exception):
+    """Indicates that the user is already logged in."""
+
+    pass
+
+
+def _load_config(config_file):
+    """Returns the configuration."""
+
+    try:
+        with open(config_file, 'r') as cfg:
+            text = cfg.read()
+    except FileNotFoundError:
+        return DEFAULT_CONFIG
+
+    return DEFAULT_CONFIG.copy().update(loads(text))
+
+
+def coerce(typ):
+    """Coerces the return type of a funcion."""
+
+    def decorator(funcion):
+        @wraps(funcion)
+        def wrap(*args, **kwargs):
+            return typ(funcion(*args, **kwargs))
+
+        return wrap
+
+    return decorator
+
+
+class LoginPolicy(Enum):
+    """Available login policies."""
+
+    MULTI = 'multi'
+    OVERRIDE = 'override'
+    SINGLE = 'deny'
+
+
+class Config(NamedTuple):
+    """The respective configuration."""
+
+    allow_root: bool
+    min_uid: int
+    login_policy: LoginPolicy
+
+
+class Session(NamedTuple):
+    """Represents a session."""
+
+    session_id: UUID
+    start: datetime
+    user: struct_passwd
+
+
+class SessionManager(dict):
+    """A web service session handler."""
+
+    def __new__(cls, **_):
+        """Returns a new session manager."""
+        super().__new__(cls)
+
+    def __init__(self, config_file=CONFIG_FILE):
+        """Sets the config_file."""
+        super().__init__()
+        self.config_file = config_file
+
+    @property
+    @lru_cache(maxsize=1)
+    def config(self):
+        """Returns the configuration."""
+        config = _load_config(self.config_file)
+        return Config(
+            config['allow_root'], config['min_uid'],
+            LoginPolicy(config['login_policy']))
+
+    @property
+    @coerce(frozenset)
+    def users(self):
+        """Yields the users."""
+        for session in self.values():
+            yield session.user
+
+    def _logout(self, user_name):
+        """Logs out a user."""
+        sessions = {
+            session_id for session_id, user in self.items()
+            if user.pw_name == user_name}
+
+        for session in sessions:
+            del self[session]
+
+    def login(self, user_name, password) -> Session:
+        """Attempts a login."""
+        try:
+            user = getpwnam(user_name)
+        except KeyError:
+            raise InvalidUserNameOrPassword()
+
+        if user.pw_name == 'root' or user.pw_uid == 0:
+            if not self.config.allow_root:
+                raise InvalidUserNameOrPassword()
+
+        if user.pw_uid < self.config.min_uid:
+            raise InvalidUserNameOrPassword()
+
+        if not authenticate(user.pw_name, password):
+            raise InvalidUserNameOrPassword()
+
+        if self.config.login_policy == LoginPolicy.DENY:
+            if user.pw_name in (user.pw_name for user in self.users):
+                raise AlreadyLoggedIn()
+        elif self.config.login_policy == LoginPolicy.OVERRIDE:
+            self._logout(user.pw_name)
+
+        session = Session(uuid4(), datetime.now(), user)
+        self[session.session_id] = session
+        return session
+
+    def close(self, session_id):
+        """Closes the respective sesion."""
+        return self.pop(session_id, None)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='httpam',
+    author='Richard Neumann',
+    author_email='<mail at richard dash neumann dot de>',
+    py_modules=['httpam'],
+    url='https://gitlab.com/coNQP/httpam',
+    license='GPLv3',
+    description='An HTTP authentication library using PAM.')
